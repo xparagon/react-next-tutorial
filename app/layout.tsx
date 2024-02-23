ace94502b80d8a57adbcccf65c835bf6080d11fe
Fred Pallesen <35865041+xparagon@users.noreply.github.com>
2024-02-23 10:03:59 +0000

    layout testing

--- app/layout.tsx
+++ app/layout.tsx
@@ -16,7 +16,13 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className + " flex min-h-screen flex-col items-center justify-between p-2"}>
+        <header>Header</header>
+        <main>
+        {children}
+        </main>
+        <footer>Footer</footer>
+        </body>
     </html>
   );
 }
